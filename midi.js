55dff563cb70d662b58c1cece830cb9a98cea7fe
koturn <jeak.koutan.apple@gmail.com>
2015-11-07 06:38:49 +0900

    Correspond to the latest specification of Web MIDI API

--- midi.js
+++ midi.js
@@ -1,96 +1,115 @@
-var midi = null;  // グローバルMIDIAccessオブジェクト
-var inputSelect = null;
-var inputList = null;
-var outputSelect = null;
-var outputList = null;
+;(function(global) {
+  'use strict';
 
-try {
-  if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess({sysex: false}).then(function(midiAccess) {
-      midi = midiAccess;
+  function Midi() {}
 
-      var inputCmb = document.getElementById('inputPort');
-      inputList = midi.inputs();
-      for (var i = 0; i < inputList.length; i++) {
-        var input = inputList[i];
-        inputCmb.appendChild(new Option(input.manufacturer + ' ' + input.name, input.id ));
+  var inputSelect = null;
+  var inputList;
+  var outputSelect = null;
+  var outputList;
+
+  try {
+    if (global.navigator.requestMIDIAccess) {
+      global.navigator.requestMIDIAccess().then(function(midiAccess) {
+        var inputCmb = global.document.getElementById('inputPort');
+        var outputCmb = global.document.getElementById('outputPort');
+
+        if (typeof midiAccess.inputs === "function") {
+          inputList = midiAccess.inputs();
+          for (var i = 0; i < inputList.length; i++) {
+            var input = inputList[i];
+            inputCmb.appendChild(new global.Option(input.manufacturer + ' ' + input.name, input.id ));
+          }
+          inputCmb.appendChild(new global.Option('NONE', -1));
+          inputSelect = inputList.length > 0 ? inputList[0] : null;
+
+          outputList = midiAccess.outputs();
+          for (var i = 0; i < outputList.length; i++) {
+            var output = outputList[i];
+            outputCmb.appendChild(new global.Option(output.manufacturer + ' ' + output.name, output.id ));
+          }
+          outputCmb.appendChild(new global.Option('NONE', -1));
+          outputSelect = outputList.length > 0 ? outputList[0] : null;
+        } else {
+          inputList = [];
+          var inputItr = midiAccess.inputs.values();
+          for (var input = inputItr.next(); !input.done; input = inputItr.next()) {
+            var v = input.value;
+            inputList[inputList.length] = v;
+            inputCmb.appendChild(new global.Option(v.manufacturer + ' ' + v.name, v.id));
+          }
+          inputCmb.appendChild(new global.Option('NONE', -1));
+          inputSelect = inputList.length > 0 ? inputList[0] : null;
+
+          outputList = [];
+          var outputItr = midiAccess.outputs.values();
+          for (var output = outputItr.next(); !output.done; output = outputItr.next()) {
+            var v = output.value;
+            outputList.push(v);
+            outputCmb.appendChild(new global.Option(v.manufacturer + ' ' + v.name, v.id));
+          }
+          outputCmb.appendChild(new global.Option('NONE', -1));
+          outputSelect = outputList.length > 0 ? outputList[0] : null;
+        }
+      },
+      function(msg) {
+        global.alert('Failed to get MIDI access - ' + msg);
+      });
+    } else {
+      global.alert('お使いのブラウザはWeb MIDI APIに対応していません。Web MIDI APIを有効化したChromeを使用してください。');
+    }
+  } catch (e) {
+    global.console.log(e);
+  }
+
+  Midi.prototype.sendNote = function(data0, data1) {
+    if (outputSelect) {
+      try {
+        outputSelect.send([data0, data1, 0x7f]);
+        outputSelect.send([0x80, data1, 0x7f], global.performance.now() + 1000.0);
+      } catch(e) {
+        global.console.log(e);
       }
-      inputCmb.appendChild(new Option('NONE', -1));
+    }
+  };
 
-      var outputCmb = document.getElementById('outputPort');
-      outputList = midi.outputs();
-      for ( var i = 0; i < outputList.length; i++ ) {
-        var output = outputList[i];
-        outputCmb.appendChild(new Option(output.manufacturer + ' ' + output.name, output.id ));
+  Midi.prototype.changeInput = function() {
+    function onmidimessage(event) {
+      try {
+        outputSelect.send(event);
+      } catch(e) {
+        global.console.log(e);
       }
-      outputCmb.appendChild(new Option('NONE', -1));
-
-      if (inputList.length > 0) changeInput();
-      if (outputList.length > 0) changeOutput();
-    },
-    function(msg) {
-      alert('Failed to get MIDI access - ' + msg );
-    });
-  } else {
-    alert('お使いのブラウザはWeb MIDI APIに対応していません。Web MIDI APIを有効化したChromeを使用してください。');
-  }
-} catch (e) {
-  console.log(e);
-}
-
-
-function echoMIDIMessage(event) {
-  if (outputSelect) {
-    try {
-      outputSelect.send(event.data);
-    } catch (e) {
-      console.log(e);
     }
-  }
-}
+    var inputCmb = global.document.getElementById('inputPort');
+    var selectIndex = inputCmb.selectedIndex;
+    var inLength = inputList.length;
 
-
-function echoMIDIMessage2(data0, data1) {
-  if (outputSelect) {
-    try {
-      outputSelect.send([data0, data1, 0x7f]);
-      outputSelect.send([0x80, data1, 0x7f], performance.now() + 1000.0);
-    } catch(e) {
-      console.log(e);
+    inputSelect = null;
+    for (var i = 0; i < inLength; i++) {
+      var input = inputList[i];
+      if (inputCmb.options[selectIndex].value === input.id) {
+        inputSelect = input;
+        inputSelect.onmidimessage = onmidimessage;
+        break;
+      }
     }
-  }
-}
+  };
 
+  Midi.prototype.changeOutput = function() {
+    var outputCmb = global.document.getElementById('outputPort');
+    var selectIndex = outputCmb.selectedIndex;
+    var outLength = outputList.length;
 
-function changeInput() {
-  var inputCmb = document.getElementById('inputPort');
-  var selectIndex = inputCmb.selectedIndex;
-  var inLength = inputList.length;
-
-  inputSelect = null;
-  input.onmidimessage = null;
-  for (var i = 0; i < inLength; i++) {
-    var input = inputList[i];
-    if (inputCmb.options[selectIndex].value == input.id) {
-      inputSelect = input;
-      inputSelect.onmidimessage = echoMIDIMessage;
-      break;
+    outputSelect = null;
+    for (var i = 0; i < outLength; i++) {
+      var output = outputList[i];
+      if (outputCmb.options[selectIndex].value === output.id) {
+        outputSelect = output;
+        break;
+      }
     }
-  }
-}
+  };
 
-
-function changeOutput() {
-  var outputCmb = document.getElementById('outputPort');
-  var selectIndex = outputCmb.selectedIndex;
-  var outLength = outputList.length;
-
-  outputSelect = null;
-  for (var i = 0; i < outLength; i++) {
-    var output = outputList[i];
-    if  (outputCmb.options[selectIndex].value == output.id) {
-      outputSelect = output;
-      break;
-    }
-  }
-}
+  global.Midi = Midi;
+})((this || 0).self || global);
